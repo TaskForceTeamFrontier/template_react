cc5031f98df1ec86b75fb0d00a5ed04ad921b319
DevB <cruiseradar@gmail.com>
2025-01-28 20:25:58 +0900

    react input type 종류류

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,121 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const [valueText, setValueText] = useState<string>("");
+    const [valueNumver, setValueNumber] = useState<number | "">("");
+    const [valueCheckBox, setValueCheckBox] = useState<boolean | false>(false);
+    const [valueRadio, setValueRadio] = useState<string>("");
+    const [valueDropDown, setValueDropDown] = useState<string>("");
+    const [valueTextArea, setValueTextArea] = useState<string>("");
+    const [valueFile, setValueFile] = useState<File | null>(null);
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length > 0) {
+        setValueFile(e.target.files[0]);
+      }
+    };
+    const [valueDate, setValueDate] = useState<string>("");
+    const [valueTime, setValueTime] = useState<string>('');
+    const handleChangeTime = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValueTime(event.target.value);
+      };
+    const [valueRange, setValueRange] = useState<number>(50);
+    const [valuePassword, setValuePassword] = useState<string>("");
+    const [valueColor, setValueColor] = useState<string>('#000000');
+    const handleChangeColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValueColor(event.target.value);
+      };
+
+    return (
+        <>
+            <div>
+                <input type="text" value={valueText}
+                    placeholder="Enter text"
+                    className="border rounded px-2 py-1"
+                    onChange={(e) => setValueText(e.target.value)}/>
+            </div>
+            <div>
+                <input type="number" value={valueNumver}
+                    placeholder="Enter number"
+                    className="border rounded px-2 py-1"
+                    onChange={(e) => setValueNumber(e.target.value === "" ? "" : Number(e.target.value))}/>
+            </div>
+            <div>
+                <label className="flex items-center gap-2">
+                    <input type="checkbox" checked={valueCheckBox}
+                        className="cursor-pointer"
+                        onChange={(e) => setValueCheckBox(e.target.checked)}/>
+                    Check
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input type="radio" value="Option 1"
+                        checked={valueRadio === "Option 1"}
+                        onChange={(e) => setValueRadio(e.target.value)}/>
+                    Option 1
+                </label>
+                <label>
+                    <input type="radio" value="Option 2"
+                        checked={valueRadio === "Option 2"}
+                        onChange={(e) => setValueRadio(e.target.value)}/>
+                    Option 2
+                </label>
+            </div>
+            <div>
+                <select value={valueDropDown}
+                    onChange={(e) => setValueDropDown(e.target.value)}
+                    className="border rounded px-2 py-1">
+                        <option value="">Choose an option</option>
+                        <option value="Opt01">Option 1</option>
+                        <option value="Opt02">Option 2</option>
+                </select>
+            </div>
+            <div>
+                <textarea value={valueTextArea}
+                    placeholder="Enter your message"
+                    rows={4}
+                    className="border rounded px-2 py-1 w-full"
+                    onChange={(e) => setValueTextArea(e.target.value)}/>
+            </div>
+            <div>
+                <input type="file" onChange={handleFileChange} />
+                {
+                    valueFile && <p>Selected File: {valueFile.name}</p>
+                }
+            </div>
+            <div>
+                <input type="date" value={valueDate}
+                    className="border rounded px-2 py-1"
+                    onChange={(e) => setValueDate(e.target.value)}/>
+            </div>
+            <div>
+                <input type="time" value={valueTime}
+                    className="border rounded px-2 py-1"
+                    onChange={handleChangeTime}/>
+            </div>
+            <div>
+                <input type="range" value={valueRange}
+                    min={0}
+                    max={100}
+                    className="w-full"
+                    onChange={(e) => setValueRange(Number(e.target.value))}/>
+                <p>Value: {valueRange}</p>
+            </div>
+            <div>
+                <input type="password" value={valuePassword}
+                    placeholder="Enter password"
+                    className="border rounded px-2 py-1"
+                    onChange={(e) => setValuePassword(e.target.value)}/>
+            </div>
+            <div>
+                <input type="color" value={valueColor}
+                    placeholder="Enter password"
+                    className="border rounded px-2 py-1"
+                    onChange={handleChangeColor}/>
+            </div>
+        </>
+    );
 }
